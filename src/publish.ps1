98e269d87b07e69f306470bbd811dd2ad25eb5bb
Shoaib Ali <alleey@gmail.com>
2024-08-30 20:11:57 +0500

    sadfsad

--- src/publish.ps1
+++ src/publish.ps1
@@ -9,12 +9,6 @@ $projDir = "App"
 Set-StrictMode -version 2.0
 $ErrorActionPreference = "Stop"
 
-# Find MSBuild.
-$msBuildPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" `
-    -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe `
-    -prerelease | select-object -first 1
-Write-Output "MSBuild: $((Get-Command $msBuildPath).Path)"
-
 # Clean output directory.
 $publishDir = "../../clickonce"
 $publishUrl = "https://alleey.github.io/preventscreenlock/#/"
@@ -29,22 +23,13 @@ Push-Location $projDir
 try
 {
     Write-Output "Working directory: $pwd"
-
-    Write-Output "Restoring:"
-    dotnet restore App.csproj -r win-x64
-
     Write-Output "Publishing:"
-    $msBuildVerbosityArg = "/v:m"
-    if ($env:CI) {
-        $msBuildVerbosityArg = ""
-    }
-
-    & $msBuildPath /target:publish `
-        /p:PublishProfile=Properties\PublishProfiles\ClickOnceProfile.pubxml `
-        /p:PublishDir=$publishDir `
-        /p:PublishUrl=$publishUrl `
-        /p:Configuration=Release `
-        $msBuildVerbosityArg App.csproj
+    dotnet publish App.csproj `
+        --configuration Release `
+        --output $publishDir `
+        -p:PublishProfile=Properties\PublishProfiles\ClickOnceProfile.pubxml `
+        -p:PublishUrl=$publishUrl `
+        -p:DebugType=none `
 }
 finally
 {
